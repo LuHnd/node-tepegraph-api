009616b44fc1a544bba07973e41f4abfaf2a359b
unknown <hennady.lukinov@gmail.com>
2020-11-08 18:15:08 +0200

    createAccount(), add doc to functions

--- src/telegraph.js
+++ src/telegraph.js
@@ -26,7 +26,32 @@ class Telegraph {
         throw this._error(err);
       });
   }
-  createAccount() {}
+  /**
+   *
+   * Use this method to create a new Telegraph account. Most users only need one account, but this can be useful for channel administrators who would like to keep individual author names and profile links for each of their channels. On success, returns an Account object with the regular fields and an additional access_token field.
+   * @param {String} short_name  - Required. Account name, helps users with several accounts remember which they are currently using. Displayed to the user above the "Edit/Publish" button on Telegra.ph, other users don't see this name.
+   * @param {Object} [options]
+   * @param {String} [options.author_name] - Default author name used when creating new articles.
+   * @param {String} [options.author_url] - Default profile link, opened when users click on the author's name below the title. Can be any link, not necessarily to a Telegram profile or channel.
+   */
+  createAccount(short_name, options = {}) {
+    return this._request("createAccount", {
+      short_name,
+      ...options,
+    });
+  }
+
+  /**
+   *
+   * Use this method to create a new Telegraph page. On success, returns a Page object.
+   * @param {String} access_token - Required. Access token of the Telegraph account.
+   * @param {String} title - Required. Page title.
+   * @param {Array} content - Required. Content of the page.
+   * @param {Object} [options]
+   * @param {String} [options.author_name] - Author name, displayed below the article's title.
+   * @param {String} [options.author_url] - Profile link, opened when users click on the author's name below the title. Can be any link, not necessarily to a Telegram profile or channel.
+   * @param {Boolean} [options.return_content] - If true, a content field will be returned in the Page object (see: Content format).
+   */
   createPage(access_token, title, content, options = {}) {
     return this._request("createPage", {
       access_token,
--- tests/post.test.js
+++ tests/post.test.js
@@ -3,6 +3,12 @@ const Telegraph = require("../src/telegraph");
 const tph = new Telegraph();
 const token = "a8c53654f596b853aa10c393771031331faa605978f67a796dbd239cb64a";
 
+test("createAccount", () => {
+  return tph.createAccount("ShortName").then(function (res) {
+    expect(res.result.short_name).toBe("ShortName");
+  });
+});
+
 test("createPage", () => {
   return tph
     .createPage(token, "title", [{ tag: "p", children: ["Test"] }])
