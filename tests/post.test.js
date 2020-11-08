7e5034fa0acbf09a204c89c59eec9ffc033b72de
unknown <hennady.lukinov@gmail.com>
2020-11-08 19:13:29 +0200

    Update post.test.js

--- tests/post.test.js
+++ tests/post.test.js
@@ -3,16 +3,32 @@ const Telegraph = require("../src/telegraph");
 const tph = new Telegraph();
 const token = "a8c53654f596b853aa10c393771031331faa605978f67a796dbd239cb64a";
 
-test("createAccount", () => {
+test("createAccount()", () => {
   return tph.createAccount("ShortName").then(function (res) {
     expect(res.result.short_name).toBe("ShortName");
   });
 });
 
-test("createPage", () => {
+test("editAccountInfo()", () => {
+  return tph
+    .editAccountInfo(token, { short_name: "edit ShortName" })
+    .then(function (res) {
+      expect(res.result.short_name).toBe("edit ShortName");
+    });
+});
+
+test("getAccountInfo()", () => {
+  return tph
+    .getAccountInfo(token, { fields: ["short_name", "author_name"] })
+    .then(function (res) {
+      expect(res.result.short_name).toBeDefined();
+    });
+});
+
+test("createPage()", () => {
   return tph
     .createPage(token, "title", [{ tag: "p", children: ["Test"] }])
     .then(function (res) {
-      expect(res.ok).toBe(true);
+      expect(res.result.title).toBe("title");
     });
 });
